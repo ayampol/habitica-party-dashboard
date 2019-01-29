f8b58c84153b9a21ef67678380ce71a9f3bbfcad
Alena Y <ayampol1@binghamton.edu>
2019-01-29 16:31:13 -0500

    Fixed formatting

--- src/main.elm
+++ src/main.elm
@@ -1,3 +1,5 @@
+module Main exposing (GetResult(..), Model, Msg(..), createAuthHeader, getPartyStatus, init, main, statDecoder, subscriptions, update, view, viewResult)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -11,12 +13,12 @@ import Json.Decode exposing (Decoder, field, string)
 
 
 main =
-  Browser.element { 
-    init = init
-  , update = update
-  , subscriptions = subscriptions
-  , view = view 
-  }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
 
@@ -24,21 +26,23 @@ main =
 
 
 type alias Model =
-  { username : String
-  , apiKey : String
-  , curStat : GetResult
-  }
+    { username : String
+    , apiKey : String
+    , curStat : GetResult
+    }
+
 
 type GetResult
-  = Failure
-  | Success String
-  | InProgress
-  
-init : () -> (Model, Cmd Msg)
+    = Failure
+    | Success String
+    | InProgress
+
+
+init : () -> ( Model, Cmd Msg )
 init _ =
-  (  Model "" "" InProgress
-    , Cmd.none 
-  )
+    ( Model "" "" InProgress
+    , Cmd.none
+    )
 
 
 
@@ -46,31 +50,31 @@ init _ =
 
 
 type Msg
-  = UpdateUsername String
-  | UpdateApikey String
-  | SubmitPair 
-  | GotStat (Result Http.Error String)
+    = UpdateUsername String
+    | UpdateApikey String
+    | SubmitPair
+    | GotStat (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    UpdateUsername username ->
-      ({ model | username = username }, Cmd.none )
+    case msg of
+        UpdateUsername username ->
+            ( { model | username = username }, Cmd.none )
 
-    UpdateApikey apiKey ->
-      ({ model | apiKey = apiKey }, Cmd.none )
+        UpdateApikey apiKey ->
+            ( { model | apiKey = apiKey }, Cmd.none )
 
-    SubmitPair ->
-      ({ model | username = String.reverse model.username }, Cmd.none )
+        SubmitPair ->
+            ( { model | username = String.reverse model.username }, Cmd.none )
 
-    GotStat result ->
-      case result of 
-        Ok url ->
-          ({model | curStat = Success url}, Cmd.none)
+        GotStat result ->
+            case result of
+                Ok url ->
+                    ( { model | curStat = Success url }, Cmd.none )
 
-        Err _ ->
-          ({model | curStat = Failure }, Cmd.none)
+                Err _ ->
+                    ( { model | curStat = Failure }, Cmd.none )
 
 
 
@@ -79,7 +83,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
 
 
 
@@ -88,68 +92,74 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ input [ placeholder "Enter username"
-    , value model.username
-    , onInput UpdateUsername ] []
-    , br [] [] 
-    , input [ type_ "password"
-    , placeholder "Enter API key"
-    , value model.apiKey
-    , onInput UpdateApikey] []
-    , div [] [
-      button [ onClick SubmitPair ] [text "sUBMIT"]
-    ] 
-    , div [] [ text (model.username) ]
-    , div [] [ text (model.apiKey)  ]
-    ]
+    div []
+        [ input
+            [ placeholder "Enter username"
+            , value model.username
+            , onInput UpdateUsername
+            ]
+            []
+        , br [] []
+        , input
+            [ type_ "password"
+            , placeholder "Enter API key"
+            , value model.apiKey
+            , onInput UpdateApikey
+            ]
+            []
+        , div []
+            [ button [ onClick SubmitPair ] [ text "sUBMIT" ]
+            ]
+        , div [] [ text model.username ]
+        , div [] [ text model.apiKey ]
+        ]
+
 
 viewResult : Model -> Html Msg
 viewResult model =
-  case model.curStat of 
-    Failure ->
-      div []
-        [ text "API request failed. "
-          , button [] [ text "Yeah, this sucked."]
-        ]
+    case model.curStat of
+        Failure ->
+            div []
+                [ text "API request failed. "
+                , button [] [ text "Yeah, this sucked." ]
+                ]
 
-    Success url ->
-      div []
-        [
-          text "look we succeed"
-        ]
+        Success url ->
+            div []
+                [ text "look we succeed"
+                ]
 
-    InProgress ->
-      div []
-        [
-          text "In progress"
-        ]
+        InProgress ->
+            div []
+                [ text "In progress"
+                ]
+
+
 
 -- HTTP
 
+
 getPartyStatus : String -> String -> Cmd Msg
-getPartyStatus username apiKey = 
-  Http.request
-    { method = "GET"
-    , headers = createAuthHeader username apiKey
-    , url = "https://habitica.com/api/v3/groups?type=party"
-    , body = Http.emptyBody
-    , expect = Http.expectString GotStat
-    , timeout = Nothing
-    , tracker = Nothing
-    }
+getPartyStatus username apiKey =
+    Http.request
+        { method = "GET"
+        , headers = createAuthHeader username apiKey
+        , url = "https://habitica.com/api/v3/groups?type=party"
+        , body = Http.emptyBody
+        , expect = Http.expectString GotStat
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
 
 statDecoder : String -> Html Msg
 statDecoder resulting =
-  div []
-    [
-      h2 [] [text "String Vomit"]
-      , text resulting
-    ]
+    div []
+        [ h2 [] [ text "String Vomit" ]
+        , text resulting
+        ]
+
 
 createAuthHeader : String -> String -> List Http.Header
 createAuthHeader username apikey =
-  [Http.header "x-api-user" username , Http.header "x-api-key" apikey ]
-  
-
-
+    [ Http.header "x-api-user" username, Http.header "x-api-key" apikey ]
