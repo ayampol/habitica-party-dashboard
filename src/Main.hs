f2e2d0305603354ea7582a214cdab0de6e2b4963
Alena Yampolskaya <ayampol1@binghamton.edu>
2019-08-02 20:58:58 -0400

    Send WS message to all clients on webhook req
    Currently sends a test message. Does not discriminate based on
    the client - should add party ids stored alongside clients to
    allow for this.

--- src/Main.hs
+++ src/Main.hs
@@ -6,9 +6,12 @@ module Main where
 import qualified Control.Concurrent as Concurrent
 import qualified Control.Exception as Exception
 import qualified Control.Monad as Monad
+import Data.Aeson
+import Data.Aeson.Types
 import qualified Data.List as List
 import qualified Data.Maybe as Maybe
 import qualified Data.Text as Text
+import Debug.Trace
 import qualified Network.HTTP.Types as Http
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
@@ -21,17 +24,48 @@ main = do
   state <- Concurrent.newMVar []
   putStrLn "Running WS server..."
   Warp.run 3000 $
-    WS.websocketsOr WS.defaultConnectionOptions (wsApp state) httpApp
+    WS.websocketsOr
+      WS.defaultConnectionOptions
+      (wsApp state)
+      (trace "response received" httpApp state)
 
-httpApp :: Wai.Application
-httpApp _ respond =
-  respond $ Wai.responseLBS Http.status400 [] "Not a websocket request"
+httpApp :: Concurrent.MVar State -> Wai.Application
+httpApp state request respond =
+  case Wai.requestMethod request of
+    "POST" -> do
+      broadcastHabitica state "bleh"
+      respond (traceShow request Wai.responseLBS Http.status200 [] "")
+    _ -> respond $ Wai.responseLBS Http.status400 [] "Not a websocket request"
 
 type ClientId = Int
 
 type Client = (ClientId, WS.Connection)
 
 type State = [Client]
+
+type Chat = [ChatMsg]
+
+testMsg :: Text.Text -> ChatMsg
+testMsg txt = ChatMsg "Me" ("Some message I wrote, there's words wow " <> txt)
+
+data Env = Env
+  { envMessageQueue :: [ChatMsg]
+  , clients :: [Client]
+  }
+
+data ChatMsg = ChatMsg
+  { author :: Text.Text
+  , text :: Text.Text
+  } deriving (Show)
+
+parseChatMsg :: Value -> Parser ChatMsg
+parseChatMsg _ = fail "expected data"
+
+instance FromJSON ChatMsg where
+  parseJson =
+    withObject "ChatMsg" $ \v ->
+      ChatMsg <$> ((v .: "chat") >>= (.: "user")) <*>
+      ((v .: "chat") >>= (.: "text"))
 
 nextId :: State -> ClientId
 nextId = Maybe.maybe 0 ((+) 1) . Safe.maximumMay . List.map fst
@@ -53,6 +87,11 @@ disconnectClient clientId stateRef =
 listen :: WS.Connection -> ClientId -> Concurrent.MVar State -> IO ()
 listen conn clientId stateRef =
   Monad.forever $ do WS.receiveData conn >>= broadcast clientId stateRef
+
+broadcastHabitica :: Concurrent.MVar State -> Text.Text -> IO ()
+broadcastHabitica stateRef msg = do
+  clients <- Concurrent.readMVar stateRef
+  Monad.forM_ clients $ \(_, conn) -> WS.sendTextData conn msg
 
 broadcast :: ClientId -> Concurrent.MVar State -> Text.Text -> IO ()
 broadcast clientId stateRef msg = do
